9f37664440d857658924000061bf0dcaf3c67a74
mkerin <m4tt.kerin@gmail.com>
2017-08-10 11:20:08 +0100

    Q4 cleaned

--- q4_builders.py
+++ q4_builders.py
@@ -5,28 +5,26 @@ from fractions import gcd
 
 def solution(n, m, d):
     """
-    Attempt at analytical solution
-    Simply move nail from min gap to
-    middle of max gap.
+    Analytical solution;
+    Simply move nail from min gap to middle of max gap.
     """
     offset_l = [0] + list(d)
     offset_r = list(d) + [n]
     diffs = [x-y for x, y in it.izip(offset_r, offset_l)]
     
-    # Find index of diffs that we'll merge..
-    # moving window width 2 with smallest sum...
+    # Find index of the two gaps to be merged.
+    # Search a moving window width 2 for the smallest sum.
     # j'th window implies merging diff j + diff j+1
     L = len(diffs)
     win_w2 = [x+y for x, y in it.izip(diffs[:L-1], diffs[1:])]
     j = win_w2.index(min(win_w2))
     
-    # find index of diffs that we'll split
-    # ie the max diff
+    # find index of maximum gap (ie the one we'll split in 2)
     k = max(diffs)
-    numerator = k // 2 * (k // 2 + k % 2) * min(win_w2)
-    denom = diffs[j] * diffs[j+1] * k
     
     # Forming an irreducible fraction
+    numerator = k // 2 * (k // 2 + k % 2) * min(win_w2)
+    denom = diffs[j] * diffs[j+1] * k
     gg = gcd(numerator, denom)
     numerator /= gg
     denom /= gg
@@ -35,7 +33,7 @@ def solution(n, m, d):
 
 if __name__ == '__main__':
     D = []
-    with open('q4_input.txt') as f:
+    with open('input/q4_input.txt') as f:
         n, m = f.readline().rstrip("\n").split()
         n, m = int(n), int(m)
         D = tuple(int(x) for x in f.readline().split())
