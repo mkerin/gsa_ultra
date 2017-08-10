7300886ff4d01f2fb93ac89b748edd5c24d1ec5d
mkerin <m4tt.kerin@gmail.com>
2017-08-10 12:16:25 +0100

    Q7 FINALLY done. misreading the question. idiot.

--- q7_shouting_array.py
+++ q7_shouting_array.py
@@ -4,37 +4,20 @@
 def solution(n):
     n_yes = 0
     
-    # 4 unique values
-    n_yes += Permute(n, 4)
+    # 4 unique values in a.
+    n_yes += nPk(n, 4)
     
-    # 3 unique values
-    # * which value to replicate
-    # * ways to place replicated value
-    n_yes += Permute(n, 3) * 3 * 1
-    
-    # 2 unique values
-    # * which value to replicate
-    # * ways to place replicated value
-    n_yes += Permute(n, 2) * 2 * 3
-    
-    # a contains only 1 unique value
-    n_yes += Permute(n, 1)
+    # 2 unique values xy in a.
+    # Valid strings: xxxy, xxyx, xyxx, yxxx
+    n_yes += nPk(n, 2) * 4
     return n_yes
 
 
-def Permute(n, k):
-    """
-    Niave implementation of nPk = n! // k!
-    Fine given that k <= 4.
-    """
+def nPk(n, k):
+    """Niave implementation of nPk = n! // k!. Fine given n, k both small."""
     if n < k:
         return 0
     res = 1
     for x in range(n-k+1, n+1):
         res *= x
     return res
-
-
-if __name__ == '__main__':
-    res = solution(3)
-    print(res)
